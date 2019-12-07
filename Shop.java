157ae1a65b4d92656d91197392b73e5a33a7aa0a
gologo-path <54819993+gologo-path@users.noreply.github.com>
2019-12-07 12:55:26 +0200

    set-get для магазина

---
+++ Shop.java
@@ -0,0 +1,15 @@
+
+public class Shop {
+    private String name;
+    private String adr;
+    private Vehicle reg[];
+    
+    public void setName(String name){this.name = name;}
+    public String getName(){return name;}
+    
+    public void setAdr(String adr){this.adr = adr;}
+    public String getAdr(){return adr;}
+    
+    public void setAutoRegistry(Vehicle[] reg){this.reg = reg;}
+    public Vehicle[] getAutoRegistry(){return reg;}
+}

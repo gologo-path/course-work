e6234745a5be861c7822ba4d63efa564b1bc47d3
STRIKER-2-0 <58944307+STRIKER-2-0@users.noreply.github.com>
2019-12-16 18:19:20 +0200

    сщььше ккк

--- Shop.java
+++ Shop.java
@@ -9,7 +9,7 @@ public class Shop {
     
     public void setAdr(String adr){this.adr = adr;}
     public String getAdr(){return adr;}
-    
+    //енененен
     public void setAutoRegistry(Vehicle[] reg){this.reg = reg;}
     public Vehicle[] getAutoRegistry(){return reg;}
 }

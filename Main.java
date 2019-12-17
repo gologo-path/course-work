fe89d925c5c7a226c172cfc6e56f044635b8f4bd
STRIKER-2-0 <58944307+STRIKER-2-0@users.noreply.github.com>
2019-12-17 21:17:39 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String []args){
+
         System.out.println(Type.AVTO.values());
+
+    	Scanner s=new Scanner(System.in);
+    	int ans;
+    	
+    	while (true) {
+    		System.out.println();
+    		ans=s.nextInt();
+		}
+
     }
     
 }

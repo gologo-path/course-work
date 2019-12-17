77ba7ea6f126c18350513bd1d155977011d301f4
STRIKER-2-0 <58944307+STRIKER-2-0@users.noreply.github.com>
2019-12-17 21:21:42 +0200

    rkbtyn

--- Client.java
+++ Client.java
@@ -4,13 +4,30 @@ public class Client {
     private int year;
     private int ID;
     
+    Client(){
+    	name = "somebody";
+        surname = "any";
+        year = 2000;
+        ID = 00000000;
+    }
     Client(String name, String surname, int year, int ID){
-        this.name = name;
+    	this.name = name;
         this.surname = surname;
         this.year = year;
         this.ID = ID;
     }
-    
+    public void setName(String name) {
+		this.name = name;
+	}
+    public void setSurname(String surname) {
+		this.surname = surname;
+	}
+    public void setYear(int year) {
+		this.year = year;
+	}
+    public void setID(int iD) {
+		ID = iD;
+	}
     public String getName(){return name;}
     public String getSurname(){return surname;}
     public int getYear(){return year;}

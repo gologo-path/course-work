e423df583c2def3895351126fe566f3d62ae61bb
STRIKER-2-0 <58944307+STRIKER-2-0@users.noreply.github.com>
2019-12-17 11:44:46 +0200

    начал писать мейн.....

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String []args){
-        System.out.println(Type.AVTO.values());
+    	Scanner s=new Scanner(System.in);
+    	
+    	
+    	while (true) {
+    		System.out.println();
+    		ans=s.NextInt();
+		}
     }
     
 }

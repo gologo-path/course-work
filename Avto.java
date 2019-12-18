298dc8ea331a1d132538617b3ccef4a314beb70b
STRIKER-2-0 <58944307+STRIKER-2-0@users.noreply.github.com>
2019-12-18 04:32:11 +0200

    Update Avto.java

--- Avto.java
+++ Avto.java
@@ -8,19 +8,19 @@ public class Avto extends Vehicle{
 		model=new Model();
 		model.name="any_model";
 		model.num=3;
-		//manufacturer="Cars&Motos";
+		manufacturer="Cars&Motos";
 		petrolType=PetrolType.GAS;		
 		capacity=1000;
 		cost=20000;
 	}
-	Avto(String name, int year, Model model, PetrolType petrolType, double capacity, double cost){
+	Avto(String name, Model model,  int year, String manufacturer, PetrolType petrolType, double capacity, double cost){
 		type=Type.AVTO;
 		this.name=name;
 		this.year=year;
 		model=new Model();
 		this.model.name=model.name;
 		this.model.num=model.num;
-		//this.manufacturer=manufacturer;
+		this.manufacturer=manufacturer;
 		this.petrolType=petrolType;
 		this.capacity=capacity;
 		this.cost=cost;

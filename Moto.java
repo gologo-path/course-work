a12ecceb41a6610c42880b63137d4c00fd9bf4d1
STRIKER-2-0 <58944307+STRIKER-2-0@users.noreply.github.com>
2019-12-18 04:31:21 +0200

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -8,19 +8,19 @@ public class Moto extends Vehicle{
 		model=new Model();
 		model.name="any_model";
 		model.num=3;
-		//manufacturer="Cars&Motos";
+		manufacturer="Cars&Motos";
 		petrolType=PetrolType.GAS;		
 		capacity=1000;
 		cost=20000;
 	}
-	Moto(String name, int year, Model model, PetrolType petrolType, double capacity, double cost){
+	Moto(String name, Model model, int year,  String manufacturer, PetrolType petrolType, double capacity, double cost){
 		type=Type.MOTO;
 		this.name=name;
 		this.year=year;
 		model=new Model();
 		this.model.name=model.name;
 		this.model.num=model.num;
-		//this.manufacturer=manufacturer;
+		this.manufacturer=manufacturer;
 		this.petrolType=petrolType;
 		this.capacity=capacity;
 		this.cost=cost;
